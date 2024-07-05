56a666a1590ff5bca3cab269bbd7dc6637b13e68
Diego Garcia Guerrero <auxiliar.soporteticsena2@supergirosnortesantander.co>
2024-07-05 09:52:03 -0500

    Multer and logs integrated

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,33 @@ import fs from "node:fs";
 import path from "node:path";
 import dotenv from "dotenv";
 import morgan from "morgan";
+import multer from "multer";
 import router from "./routes";
 
 dotenv.config();
+
+const storage = multer.diskStorage({
+  destination: "uploads/",
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    const ext = path.extname(file.originalname);
+
+    cb(null, file.fieldname + "-" + uniqueSuffix + ext);
+  },
+});
+
+const whitelist = ["image/png", "image/jpeg", "image/jpg", "image/webp"];
+
+const upload = multer({
+  storage,
+  fileFilter(req, file, cb) {
+    if (!whitelist.includes(file.mimetype)) {
+      return cb(new Error("El tipo del archivo " + file.originalname + " no está permitido"));
+    }
+
+    cb(null, true);
+  },
+});
 
 const app: Express = express();
 const port = process.env.PORT || 3000;
@@ -22,6 +46,10 @@ app.use(
   })
 );
 app.use(router);
+
+app.post("/upload", upload.array("file"), (req, res) => {
+  return res.json({ message: "Upload success" });
+});
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port} :PPP`);
